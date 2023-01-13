f140ff987db4e0b414d410cff2adab990d0aed1f
seanf316 <seanfinn316@gmail.com>
2023-01-13 01:03:51 +0000

    Updated apply_extension method

---
+++ student.py
@@ -0,0 +1,30 @@
+from datetime import date, timedelta
+
+
+class Student:
+    """A Student class as base for method testing"""
+
+    def __init__(self, first_name, last_name):
+        self._first_name = first_name
+        self._last_name = last_name
+        self._start_date = date.today()
+        self.end_date = date.today() + timedelta(days=365)
+        self.naughty_list = False
+
+    @property
+    def full_name(self):
+        return f"{self._first_name} {self._last_name}"
+
+    @property
+    def email(self):
+        return f"{self._first_name}.{self._last_name}@email.com"
+
+    def alert_santa(self):
+        self.naughty_list = True
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_student.py
@@ -0,0 +1,61 @@
+import unittest
+from student import Student
+from datetime import timedelta
+
+
+class TestStudent(unittest.TestCase):
+    """
+    * Create a new test method called test_apply_extension
+    * Inside test_apply_extension, store the current end_date for our student
+    * instance in avariabkle called old_end_date
+    * Call a method named apply_extension that will take a number of days as
+    * an argument on the student instance to update the end_date
+    * Assert whether the instance's end_date equals the old end date plus the
+    * days argument that was passed in using timedelta
+    * Run the tests to confirm that the new method is failing
+    * In the Student class, create a new method called apply_extension that
+    * has a parameter calles days
+    * Use the timedelta medthod from datetime to update the end_date property
+    * Run the tests to confirm they are working
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("tearDownClass")
+
+    def setUp(self):
+        print("setUp")
+        self.student = Student("John", "Doe")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_full_name(self):
+        print("test_full_name")
+        self.assertEqual(self.student.full_name, "John Doe")
+
+    def test_alert_santa(self):
+        print("test_alert_santa")
+        self.student.alert_santa()
+
+        self.assertTrue(self.student.naughty_list)
+
+    def test_email(self):
+        print("test_email")
+        self.assertEqual(self.student.email, "John.Doe@email.com")
+
+    def test_apply_extension(self):
+        old_end_date = self.student.end_date
+        self.student.apply_extension(5)
+
+        self.assertEqual(
+            self.student.end_date, old_end_date + timedelta(days=5)
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
